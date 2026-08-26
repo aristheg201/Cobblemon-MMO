2861fc069bad393d8592c496b7591349fef98114
aristheg201 <arisgrindel@gmail.com>
2026-08-26 08:30:46 +0700

    SVFrameLib: restore VariableRegistry access contract

--- SVFrameLib/src/main/java/io/lumine/mythic/lib/script/variable/VariableRegistry.java
+++ SVFrameLib/src/main/java/io/lumine/mythic/lib/script/variable/VariableRegistry.java
@@ -1,1 +1,15 @@
-package io.lumine.mythic.lib.script.variable;public interface VariableRegistry<T extends Variable<?>>{Variable<?> get(T variable,String path);String stringify(T variable);}
+package io.lumine.mythic.lib.script.variable;
+
+/** Registry used to resolve nested MythicLib script variables. */
+public interface VariableRegistry<T> {
+    Variable<?> accessVariable(T variable, String path);
+
+    /** Compatibility alias retained for the early Fabric source surface. */
+    default Variable<?> get(T variable, String path) {
+        return accessVariable(variable, path);
+    }
+
+    default String stringify(T variable) {
+        return String.valueOf(variable);
+    }
+}
