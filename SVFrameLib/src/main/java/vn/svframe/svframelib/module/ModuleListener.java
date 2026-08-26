4866c31f25cf80f751ba4d33b155ae4954d60711
aristheg201 <arisgrindel@gmail.com>
2026-08-26 21:37:51 +0700

    SVFrameLib: restore module listener annotation contract [skip ci]

--- SVFrameLib/src/main/java/vn/svframe/svframelib/module/ModuleListener.java
+++ SVFrameLib/src/main/java/vn/svframe/svframelib/module/ModuleListener.java
@@ -1,1 +1,11 @@
-package vn.svframe.svframelib.module;import java.lang.annotation.*;@Retention(RetentionPolicy.RUNTIME)@Target(ElementType.TYPE)public @interface ModuleListener{}
+package vn.svframe.svframelib.module;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/** Marks native module listener/toggle fields exactly like MythicLib 1.7.1. */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+public @interface ModuleListener { }
