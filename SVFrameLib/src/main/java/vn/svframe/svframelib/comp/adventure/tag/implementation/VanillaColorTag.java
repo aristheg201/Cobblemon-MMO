a85816fb253ada0f4eef9a14500c9f5f32509e2d
aristheg201 <arisgrindel@gmail.com>
2026-08-26 18:12:38 +0700

    SVFrameLib: add vanilla color tag [skip ci]

---
+++ SVFrameLib/src/main/java/vn/svframe/svframelib/comp/adventure/tag/implementation/VanillaColorTag.java
@@ -0,0 +1,3 @@
+package vn.svframe.svframelib.comp.adventure.tag.implementation;
+import vn.svframe.svframelib.comp.adventure.resolver.implementation.VanillaColorResolver;import vn.svframe.svframelib.comp.adventure.tag.AdventureTag;
+public class VanillaColorTag extends AdventureTag { public VanillaColorTag(){super("black",new VanillaColorResolver(),true,true,"dark_blue","dark_green","dark_aqua","dark_red","dark_purple","gold","gray","dark_gray","blue","green","aqua","red","light_purple","yellow","white");} }
