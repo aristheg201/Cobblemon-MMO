d92590344e19d0d005a5b83a50ffdad0de092076
aristheg201 <arisgrindel@gmail.com>
2026-08-26 18:11:59 +0700

    SVFrameLib: add gradient tag [skip ci]

---
+++ SVFrameLib/src/main/java/vn/svframe/svframelib/comp/adventure/tag/implementation/GradientTag.java
@@ -0,0 +1,3 @@
+package vn.svframe.svframelib.comp.adventure.tag.implementation;
+import vn.svframe.svframelib.comp.adventure.resolver.implementation.GradientResolver;import vn.svframe.svframelib.comp.adventure.tag.AdventureTag;
+public class GradientTag extends AdventureTag { public GradientTag(){super("gradient",new GradientResolver(),false,true);} }
