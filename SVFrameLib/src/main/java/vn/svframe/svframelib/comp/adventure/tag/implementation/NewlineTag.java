8f3a010c5db93f8ce71cd11d0ade793477cd327e
aristheg201 <arisgrindel@gmail.com>
2026-08-26 18:12:16 +0700

    SVFrameLib: add newline tag [skip ci]

---
+++ SVFrameLib/src/main/java/vn/svframe/svframelib/comp/adventure/tag/implementation/NewlineTag.java
@@ -0,0 +1,3 @@
+package vn.svframe.svframelib.comp.adventure.tag.implementation;
+import vn.svframe.svframelib.comp.adventure.tag.AdventureTag;
+public class NewlineTag extends AdventureTag { public NewlineTag(){super("newline",(src,args)->"\n",true,false,"br");} }
