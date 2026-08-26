083cc839344bd2ffad4a4f8f05683f723df160cd
aristheg201 <arisgrindel@gmail.com>
2026-08-26 18:11:52 +0700

    SVFrameLib: add adventure color tag [skip ci]

---
+++ SVFrameLib/src/main/java/vn/svframe/svframelib/comp/adventure/tag/implementation/AdventureColorTag.java
@@ -0,0 +1,3 @@
+package vn.svframe.svframelib.comp.adventure.tag.implementation;
+import vn.svframe.svframelib.comp.adventure.resolver.implementation.AdventureColorResolver;import vn.svframe.svframelib.comp.adventure.tag.AdventureTag;
+public class AdventureColorTag extends AdventureTag { public AdventureColorTag(){super("color",new AdventureColorResolver(),true,true);} }
