879cd35e69ff1db13b27fcabd498d008a07ac6ed
aristheg201 <arisgrindel@gmail.com>
2026-08-26 18:12:45 +0700

    SVFrameLib: add bold tag [skip ci]

---
+++ SVFrameLib/src/main/java/vn/svframe/svframelib/comp/adventure/tag/implementation/decorations/BoldTag.java
@@ -0,0 +1,1 @@
+package vn.svframe.svframelib.comp.adventure.tag.implementation.decorations;import vn.svframe.svframelib.comp.adventure.tag.AdventureTag;public class BoldTag extends AdventureTag{public BoldTag(){super("bold",(src,args)->"§l",true,false,"b");}}
