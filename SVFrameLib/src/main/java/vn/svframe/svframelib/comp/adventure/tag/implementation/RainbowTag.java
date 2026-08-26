5c56132698339946f711e6a2910db9b9e670100b
aristheg201 <arisgrindel@gmail.com>
2026-08-26 18:12:24 +0700

    SVFrameLib: add rainbow tag [skip ci]

---
+++ SVFrameLib/src/main/java/vn/svframe/svframelib/comp/adventure/tag/implementation/RainbowTag.java
@@ -0,0 +1,3 @@
+package vn.svframe.svframelib.comp.adventure.tag.implementation;
+import vn.svframe.svframelib.comp.adventure.resolver.implementation.RainbowResolver;import vn.svframe.svframelib.comp.adventure.tag.AdventureTag;
+public class RainbowTag extends AdventureTag { public RainbowTag(){super("rainbow",new RainbowResolver(),false,true);} }
