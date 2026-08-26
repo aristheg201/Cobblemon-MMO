b50733bcd9b2ce72eb210a1334767856255b6f9e
aristheg201 <arisgrindel@gmail.com>
2026-08-26 07:26:41 +0700

    SVFrameLib: restore native AttackHandler API

---
+++ SVFrameLib/src/main/java/io/lumine/mythic/lib/damage/AttackHandler.java
@@ -0,0 +1,10 @@
+package io.lumine.mythic.lib.damage;
+
+import net.minecraft.entity.Entity;
+
+/** Native attack-source adapter contract. */
+public interface AttackHandler {
+    default AttackMetadata getAttack(Entity entity) { return null; }
+    default boolean isAttacked(Entity entity) { return getAttack(entity) != null; }
+    default boolean isFake(Object event) { return false; }
+}
