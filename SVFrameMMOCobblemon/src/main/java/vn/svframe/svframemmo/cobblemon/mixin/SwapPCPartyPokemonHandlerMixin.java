39d085f88e92beb843d5e80d7e8218b040c010de
aristheg201 <arisgrindel@gmail.com>
2026-08-28 12:15:09 +0700

    Cobblemon integration: fix storage lock mixin descriptors

--- SVFrameMMOCobblemon/src/main/java/vn/svframe/svframemmo/cobblemon/mixin/SwapPCPartyPokemonHandlerMixin.java
+++ SVFrameMMOCobblemon/src/main/java/vn/svframe/svframemmo/cobblemon/mixin/SwapPCPartyPokemonHandlerMixin.java
@@ -12,7 +12,7 @@ import vn.svframe.svframemmo.cobblemon.SVFrameMMOCobblemon;
 
 @Mixin(value = SwapPCPartyPokemonHandler.class, remap = false)
 abstract class SwapPCPartyPokemonHandlerMixin {
-    @Inject(method = "handle(Lcom/cobblemon/mod/common/net/messages/server/storage/SwapPCPartyPokemonPacket;Lnet/minecraft/server/MinecraftServer;Lnet/minecraft/class_3222;)V", at = @At("HEAD"), cancellable = true, remap = false)
+    @Inject(method = "handle(Lcom/cobblemon/mod/common/net/messages/server/storage/SwapPCPartyPokemonPacket;Lnet/minecraft/server/MinecraftServer;Lnet/minecraft/server/network/ServerPlayerEntity;)V", at = @At("HEAD"), cancellable = true, remap = false)
     private void svframe$lockFusionPokemon(SwapPCPartyPokemonPacket packet, MinecraftServer server, ServerPlayerEntity player, CallbackInfo ci) {
         if (SVFrameMMOCobblemon.fusions().isPokemonLocked(packet.getPartyPokemonID())
                 || SVFrameMMOCobblemon.fusions().isPokemonLocked(packet.getPcPokemonID())) ci.cancel();
